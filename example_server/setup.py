78aa4ee9950a19a619ad55af867f5517a669f0cf
Balazs Hamorszky <balazs.hamorszky@hellmann.com>
2021-09-03 03:09:51 +0200

    build

--- example_server/setup.py
+++ example_server/setup.py
@@ -1,12 +1,21 @@
-from setuptools import setup
+import os
+
+from setuptools import setup, find_packages
+
+version_file = open(os.path.join('..', 'VERSION'))
 
 setup(
   name="example_server",
+  version=version_file.read().strip(),
   install_requires=[
     'jsonpickle == 2.0.0'
   ],
-  packages=['example_server'],
+  # packages=['example_server'],
   entry_points={
     "console_scripts": ["example-server=example_server.__main__:main"]
-  }
+  },
+  packages=find_packages(
+    where='src'
+  ),
+  package_dir={"": "src"}
 )
